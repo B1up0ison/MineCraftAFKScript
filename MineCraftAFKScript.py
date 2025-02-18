b83213ae1bc52a715cef93dc79f05335b8ed8f89
B1up0ison <salmanhashim@hotmail.ca>
2025-02-18 12:41:35 -0500

    Update MineCraftAFKScript.py
    -Fixed comments
    -Fixed timings

--- MineCraftAFKScript.py
+++ MineCraftAFKScript.py
@@ -1,12 +1,12 @@
 import time
 import pydirectinput
-seconds = 326 #minecraft day = 650 so run ~half
+seconds = 326 #~Half of minecraft day
 while True:
         for i in range(seconds,0,-1):
                 print(i, "seconds left")
                 time.sleep(1) #buffer
-        time.sleep(2)
-        pydirectinput.moveTo(950,750) #move cursor down to bed on minecraft windowed mode
-        time.sleep(2) 
-        pydirectinput.rightClick()  #right click on bed to sleep
-        time.sleep(7) #rougly amount of time to sleep in bed
+        time.sleep(1)
+        pydirectinput.moveTo(950,750) #Align cursor to bed
+        time.sleep(1) 
+        pydirectinput.rightClick()  #Right click on bed sleep
+        time.sleep(5) #Amount of time to slee in bed
